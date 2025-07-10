175c7c26a5b57a11b8616485398b8dca89f175c7
Milena Agostinho <milena.mba@gmail.com>
2025-07-09 21:31:53 -0300

    feat: add rooms listing

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
+import {
+    serializerCompiler,
+    validatorCompiler,
+    type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { env } from './env.ts'
+import { getRoomsRoute } from './db/http/routes/get-rooms.ts'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+    origin: '*'
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.get('/health', () => {
+    return 'OK'
+})
+
+app.register(getRoomsRoute)
+
+app.listen( { port: env.PORT })
